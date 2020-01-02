9159dae1c1ceacc25a14c1254ba0655715cc3153
Rajat <rajatdhoot123@gmail.com>
2020-01-02 21:55:07 +0530

    Updates

--- precache-manifest.b8134f1f4049af358985c3b8aeea7696.js
+++ precache-manifest.b8134f1f4049af358985c3b8aeea7696.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "42575e8c52af236538ceb68de088878b",
+    "revision": "0b9969f29044bac454b2a40204b6bbaa",
     "url": "/rajatdhoot/index.html"
   },
   {
-    "revision": "c095b9edb3c1bdf0306b",
+    "revision": "2e9bbf710ff9fcde2fae",
     "url": "/rajatdhoot/static/css/main.f63b4555.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/rajatdhoot/static/js/2.8af7bcd5.chunk.js.LICENSE"
   },
   {
-    "revision": "c095b9edb3c1bdf0306b",
-    "url": "/rajatdhoot/static/js/main.19b3c901.chunk.js"
+    "revision": "2e9bbf710ff9fcde2fae",
+    "url": "/rajatdhoot/static/js/main.c2655fbe.chunk.js"
   },
   {
     "revision": "6e51a6d59274d1327505",
