5a9a5a1416a96bb983079eec501f031f6721f5a9
Dhoot <dhoot@Dhoots-MacBook-Pro.local>
2021-05-25 23:26:21 +0530

    Portfolio page {WIP}

--- .next/server/pages/_app.js
+++ .next/server/pages/_app.js
@@ -1,73 +1,46 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
 (function() {
 var exports = {};
-exports.id = 888;
-exports.ids = [888];
+exports.id = "pages/_app";
+exports.ids = ["pages/_app"];
 exports.modules = {
 
-/***/ 973:
+/***/ "./src/pages/_app.js":
+/*!***************************!*\
+  !*** ./src/pages/_app.js ***!
+  \***************************/
 /***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-// ESM COMPAT FLAG
-__webpack_require__.r(__webpack_exports__);
-
-// EXPORTS
-__webpack_require__.d(__webpack_exports__, {
-  "default": function() { return /* binding */ _app; }
-});
-
-// EXTERNAL MODULE: external "react"
-var external_react_ = __webpack_require__(297);
-var external_react_default = /*#__PURE__*/__webpack_require__.n(external_react_);
-// EXTERNAL MODULE: external "styled-components"
-var external_styled_components_ = __webpack_require__(914);
-;// CONCATENATED MODULE: ./src/styles/global.js
-
-const GlobalStyle = external_styled_components_.createGlobalStyle`
-body {
-  margin: 0;
-  font-family: "Averta";
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-}
-
-@font-face {
-  font-family: "Averta";
-  src: local("Averta"),
-    url(./fonts/Averta/AvertaDemoPE-Regular.otf) format("woff");
-}
-`;
-/* harmony default export */ var global = ((/* unused pure expression or super */ null && (GlobalStyle)));
-;// CONCATENATED MODULE: ./src/pages/_app.js
-
-var __jsx = (external_react_default()).createElement;
-
-
-
-function MyApp({
-  Component,
-  pageProps
-}) {
-  return __jsx(Component, pageProps);
-}
-
-/* harmony default export */ var _app = (MyApp);
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ \"react\");\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var _styles_index_css__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../styles/index.css */ \"./src/styles/index.css\");\n/* harmony import */ var _styles_index_css__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_styles_index_css__WEBPACK_IMPORTED_MODULE_1__);\nvar _jsxFileName = \"/Users/dhoot/projects/rajatdhoot/src/pages/_app.js\";\n\nvar __jsx = (react__WEBPACK_IMPORTED_MODULE_0___default().createElement);\n\nfunction _extends() { _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }\n\n\n\nfunction MyApp({\n  Component,\n  pageProps\n}) {\n  return __jsx(Component, _extends({}, pageProps, {\n    __self: this,\n    __source: {\n      fileName: _jsxFileName,\n      lineNumber: 3,\n      columnNumber: 10\n    }\n  }));\n}\n\n/* harmony default export */ __webpack_exports__[\"default\"] = (MyApp);//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9yZXN1bWUvLi9zcmMvcGFnZXMvX2FwcC5qcz8yMjU0Il0sIm5hbWVzIjpbIk15QXBwIiwiQ29tcG9uZW50IiwicGFnZVByb3BzIl0sIm1hcHBpbmdzIjoiOzs7Ozs7Ozs7OztBQUFBOztBQUNBLFNBQVNBLEtBQVQsQ0FBZTtBQUFFQyxXQUFGO0FBQWFDO0FBQWIsQ0FBZixFQUF5QztBQUN2QyxTQUFPLE1BQUMsU0FBRCxlQUFlQSxTQUFmO0FBQUE7QUFBQTtBQUFBO0FBQUE7QUFBQTtBQUFBO0FBQUEsS0FBUDtBQUNEOztBQUVELCtEQUFlRixLQUFmIiwiZmlsZSI6Ii4vc3JjL3BhZ2VzL19hcHAuanMuanMiLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQgXCIuLi9zdHlsZXMvaW5kZXguY3NzXCI7XG5mdW5jdGlvbiBNeUFwcCh7IENvbXBvbmVudCwgcGFnZVByb3BzIH0pIHtcbiAgcmV0dXJuIDxDb21wb25lbnQgey4uLnBhZ2VQcm9wc30gLz47XG59XG5cbmV4cG9ydCBkZWZhdWx0IE15QXBwO1xuIl0sInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///./src/pages/_app.js\n");
 
 /***/ }),
 
-/***/ 297:
+/***/ "./src/styles/index.css":
+/*!******************************!*\
+  !*** ./src/styles/index.css ***!
+  \******************************/
+/***/ (function() {
+
+
+
+/***/ }),
+
+/***/ "react":
+/*!************************!*\
+  !*** external "react" ***!
+  \************************/
 /***/ (function(module) {
 
 "use strict";
 module.exports = require("react");;
-
-/***/ }),
-
-/***/ 914:
-/***/ (function(module) {
-
-"use strict";
-module.exports = require("styled-components");;
 
 /***/ })
 
@@ -78,7 +51,7 @@ module.exports = require("styled-components");;
 var __webpack_require__ = require("../webpack-runtime.js");
 __webpack_require__.C(exports);
 var __webpack_exec__ = function(moduleId) { return __webpack_require__(__webpack_require__.s = moduleId); }
-var __webpack_exports__ = (__webpack_exec__(973));
+var __webpack_exports__ = (__webpack_exec__("./src/pages/_app.js"));
 module.exports = __webpack_exports__;
 
 })();
--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,5 +1,119 @@
+const WORK = [
+  {
+    duration: "Feb 2020 - Present | India",
+    name: "Kukufm",
+    work: [
+      "Ownership of consumer facing product kukufm.com",
+      "Migrate site from client side to server side using Next js",
+      "Build all the product from scratch.",
+      "Responsible for end to end (developing to deployment)",
+      "Develop many campains to boost product growth",
+    ],
+  },
+  {
+    duration: "Feb 2020 - Present | India",
+    name: "Zomato",
+    work: [
+      "Ownership of consumer facing product kukufm.com",
+      "Migrate site from client side to server side using Next js",
+      "Build all the product from scratch.",
+      "Responsible for end to end (developing to deployment)",
+      "Develop many campains to boost product growth",
+    ],
+  },
+  {
+    duration: "Feb 2020 - Present | India",
+    name: "Letsventure",
+    work: [
+      "Ownership of consumer facing product kukufm.com",
+      "Migrate site from client side to server side using Next js",
+      "Build all the product from scratch.",
+      "Responsible for end to end (developing to deployment)",
+      "Develop many campains to boost product growth",
+    ],
+  },
+];
+
+const CompanyBox = ({ name, duration, work }) => (
+  <>
+    <div style={{ display: "flex", alignItems: "center" }}>
+      <h3>{name}</h3>
+      <div style={{ margin: "10px" }}>|</div>
+      <span>Senior Frontend Developer</span>
+    </div>
+    <h4>{duration}</h4>
+    <ul class="no-style-ul">
+      {work.map((wr) => (
+        <li key={wr}>{wr}</li>
+      ))}
+    </ul>
+  </>
+);
+
 const Portfolio = () => {
-  return <div>Portfolio</div>;
+  return (
+    <div>
+      <div style={{ textAlign: "center", margin: "10px" }}>
+        <h1 style={{ margin: "0px" }}>Rajat Dhoot</h1>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <span>
+            <p>
+              <a href="mailto:someone@example.com">
+                Email: rajatdhoot123@gmail.com
+              </a>
+            </p>
+          </span>
+          <span style={{ margin: "5px" }}>|</span>
+          <span>
+            <a href="tel:+918855866494">Ph: +91 8855866494</a>
+          </span>
+        </div>
+      </div>
+      <div style={{ width: "100%", display: "flex" }}>
+        <div style={{ width: "30%" }}>
+          <div>
+            <h2>Education</h2>
+            <h3 style={{ margin: "0" }}>University</h3>
+            <h4>Bachlers of Engg</h4>
+            <h4>Computer Scinence</h4>
+            <h4>Pune University June 2016 | India</h4>
+          </div>
+          <div>
+            <h2>Links</h2>
+            <ul class="no-style-ul">
+              <li>
+                <a href="#" target="_blank" rel="noopener noreferrer">
+                  Linkedin: https://www.linkedin.com/in/rajatdhoot
+                </a>
+              </li>
+              <li>
+                <a href="#" target="_blank" rel="noopener noreferrer">
+                  Github: https://github.com/rajatdhoot123
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div style={{ width: "70%" }}>
+          <h2>Work Experience</h2>
+          {WORK.map(({ name, duration, work = [] }) => (
+            <CompanyBox
+              key={name}
+              work={work}
+              name={name}
+              duration={duration}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Portfolio;
