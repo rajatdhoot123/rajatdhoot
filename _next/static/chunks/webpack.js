2bdf2bf731644ac539cc25da046ea242cb83be64
Dhoot <dhoot@Dhoots-MacBook-Pro.local>
2021-06-01 14:19:29 +0530

    Updates

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5ae32cd81e0e2932b08f"; }
+/******/ 		__webpack_require__.h = function() { return "92bfa280401b92fc2b21"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
